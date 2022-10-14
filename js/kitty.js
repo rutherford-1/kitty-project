6eae0e2bd51f110e1d771c08cba1a34828ec7a26
rutherford-1 <beth.mkrutherford@gmail.com>
2022-10-14 12:05:32 -0700

    Initial commit

---
+++ js/kitty.js
@@ -0,0 +1,76 @@
+var timer = null;
+var countdownNumber = 3;
+
+var changeState = function(state) {
+	document.body.className = 'body-state'+state;
+	clearInterval(timer);
+	countdownNumber = 3;
+	document.getElementById('countdown1').innerHTML = countdownNumber; 
+	document.getElementById('countdown2').innerHTML = countdownNumber;	
+
+	//chose yarn- countdown
+	if (state==3) {
+		countdownFunction(5,1);
+	}
+
+	//chose mouse- countdown
+	else if (state==4) {
+		countdownFunction(6,2);
+	}
+
+	//results of kitty play
+	else if (state==5) {
+		var success=setTimeout( function() {
+			var rand = Math.round(Math.random()*10);
+			if (rand > 5) {
+				changeState(7);} // success-yarn
+			else if (rand > 2) {
+				changeState(8);} //tangled-yarn
+			else {
+				changeState(11);} //bored
+		},2000);
+	}
+
+	else if (state==6){
+		var success=setTimeout( function() {
+			var rand = Math.round(Math.random()*10);
+			if (rand > 5) {
+				changeState(9);} // success-mouse
+			else if (rand > 2) {
+				changeState(10);} //under table-mouse
+			else {
+				changeState(11);} //bored
+		},2000);
+	}
+
+	else if (state==7) {
+		//do something
+	}
+	
+}
+
+var countdownFunction = function(state,countID) {
+	timer = setInterval( function() {
+		countdownNumber = countdownNumber - 1;
+		document.getElementById('countdown'+countID).innerHTML = countdownNumber;
+		if (countdownNumber <= 0) {
+			changeState(state);
+		}
+	}, 1000);
+}
+
+var helpYarn = function() {
+	var rand = Math.round(Math.random()*10);
+	if (rand > 5) {
+		changeState(7);} // success-yarn
+	else {
+		changeState(11);} //bored
+}
+
+var helpMouse = function() {
+	var rand = Math.round(Math.random()*10);
+	if (rand > 5) {
+		changeState(9);} // success-mouse
+	else {
+		changeState(11);} //bored
+}

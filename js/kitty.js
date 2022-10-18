3d635174410344c3069a223bddfe006f0c1d75b8
rutherford-1 <beth.mkrutherford@gmail.com>
2022-10-18 12:19:24 -0700

    Styled the website

--- js/kitty.js
+++ js/kitty.js
@@ -22,7 +22,7 @@ var changeState = function(state) {
 	else if (state==5) {
 		var success=setTimeout( function() {
 			var rand = Math.round(Math.random()*10);
-			if (rand > 10) {
+			if (rand > 5) {
 				changeState(7);} // success-yarn
 			else if (rand > 2) {
 				changeState(8);} //tangled-yarn
@@ -43,8 +43,25 @@ var changeState = function(state) {
 		},2000);
 	}
 
-	else if (state==7) {
-		//do something
+	if (state==8 || state==10) {
+		document.getElementById('help').className = "help show";
+	}
+	else {
+		document.getElementById('help').className = "help";
+	}
+
+	if (state==11) {
+		document.getElementById('zzz').className = "zzz show";
+	}
+	else {
+		document.getElementById('zzz').className = "zzz";
+	}
+
+	if (state==12 || state==13) {
+		document.getElementById('thank-you').className = "thank-you show";
+	}
+	else {
+		document.getElementById('thank-you').className = "thank-you";
 	}
 	
 }
@@ -53,8 +70,14 @@ var countdownFunction = function(state,countID) {
 	timer = setInterval( function() {
 		countdownNumber = countdownNumber - 1;
 		document.getElementById('countdown'+countID).innerHTML = countdownNumber;
+		if (countdownNumber < 3 && countdownNumber > 0) {
+			document.getElementById('lets-play').className = "lets-play show";
+		}
+		else {
+			document.getElementById('lets-play').className = "lets-play";
+		}
 		if (countdownNumber <= 0) {
 			changeState(state);
 		}
-	}, 1000);
+	}, 800);
 }
